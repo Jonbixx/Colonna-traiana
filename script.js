d26a355ed24e9df9d4de889416bccead699ad350
Jonbixx <111303874+Jonbixx@users.noreply.github.com>
2024-05-12 10:47:59 +0200

    rimozione img

--- script.js
+++ script.js
@@ -4,13 +4,3 @@ btn.addEventListener("click", () => {
     window.scroll(top)
 });
 
-const slideValue = document.querySelector("span");
-      const inputSlider = document.querySelector("input");
-      inputSlider.oninput = (()=>{
-        let value = inputSlider.value;
-        slideValue.textContent = value;
-        slideValue.classList.add("show");
-      });
-      inputSlider.onblur = (()=>{
-        slideValue.classList.remove("show");
-      });
